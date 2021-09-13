40e6eea2b639f2416c7212fc8dd0e0626a18d555
Nasirul-Islam <nasirulislam866@gmail.com>
2021-09-13 08:17:21 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = parseFloat(total.toFixed(2));
 };
 
 // set innerText function
@@ -61,7 +61,6 @@ const setInnerText = (id, value) => {
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
-  console.log(priceConverted);
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
