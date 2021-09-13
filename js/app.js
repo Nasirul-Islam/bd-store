c4f255ebf4bbf997aa35bde754a9955ab8a19032
Nasirul-Islam <nasirulislam866@gmail.com>
2021-09-13 16:43:25 +0600

    style

--- js/app.js
+++ js/app.js
@@ -19,11 +19,12 @@ const showProducts = (products) => {
       <div>
         <img class="product-image" src=${image}></img>
       </div>
-        <h3>${product.title}</h3>
-        <p>Category: ${product.category}</p>
+        <h3 class="product-name">${product.title}</h3>
+        <p class="category">Category: ${product.category}</p>
         <h2>Price: $ ${product.price}</h2>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"  class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button>
+        <h5 class="rating-count">Rating : <span class="ratingColor">${product.rating.rate}</span> &nbsp &nbsp Count : <span class="ratingColor">${product.rating?.count}</span></h5>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"  class="buy-now btn btn-primary">add to cart</button>
+        <button id="details-btn" class="btn btnColor">Details</button>
     </div>
     `;
     document.getElementById("all-products").appendChild(div);
